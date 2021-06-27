3deeaa83b8f1eca8fec160bfae78f26c477938fe
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-06-27 12:06:57 +0100

    init

---
+++ src/aoc2019/aoc2019.clj
@@ -0,0 +1,6 @@
+(ns aoc2019.aoc2019)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (prn x "Hello, World!"))
---
+++ test/aoc2019/aoc2019_test.clj
@@ -0,0 +1,7 @@
+(ns aoc2019.aoc2019-test
+  (:require [clojure.test :refer :all]
+            [aoc2019.aoc2019 :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
