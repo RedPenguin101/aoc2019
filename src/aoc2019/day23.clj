527a270464ee26dbb36a3d72bc1f7c47e683e38a
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-08-27 19:45:51 +0100

    day23 part2

--- src/aoc2019/day23.clj
+++ src/aoc2019/day23.clj
@@ -6,7 +6,6 @@
 (defn run-packet [computers [addr x y]]
   (update computers addr #(-> % (resume x) (resume y))))
 
-(def debug (atom nil))
 
 (defn run [computers queue]
   (cond
@@ -22,8 +21,35 @@
              []))
   ;; "Elapsed time: 214.224593 msecs"
   ;; (255 79967 15662)
-  (second @debug))
+  )
+
+(defn send-1s [cs]
+  (let [ncs (mapv #(resume % -1) cs)]
+    [ncs (vec (partition 3 (mapcat :outputs ncs)))]))
+
+(defn proc-pck [cs [[addr x y] & q]]
+  (let [new-cs (update cs addr #(-> % (resume x) (resume y)))]
+    [new-cs
+     (concat q (partition 3 (get-in new-cs [addr :outputs])))]))
+
+(defn run2
+  "packet addr 255?                    update NAT
+   packet?                         process packet
+   No cycle?                             Send -1s
+   last-nat not repeat?                  Fire NAT
+   Else                                 Terminate"
+  ([cs] (run2 [cs []] false nil nil))
+  ([[cs [pck & rst :as q]] cycle nat p-nat]
+   (cond
+     (= 255 (first pck))     (recur [cs rst] false (rest pck) p-nat)
+     pck                     (recur (proc-pck cs q) false nat p-nat)
+     (not cycle)             (recur (send-1s cs) true nat p-nat)
+     (not= (last nat) p-nat) (recur (proc-pck cs [(into [0] nat)]) false nat (last nat))
+     :else                   p-nat)))
 
 
-
-
+(comment
+  (time (run2 (mapv #(run-program2 program [%]) (range 50))))
+  ;; "Elapsed time: 3594.155488 msecs"
+  ;; 10854
+  )
